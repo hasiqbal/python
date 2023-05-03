e12df94f0bbda727c8950d99b4f15ac63a9733e6
hasiqbal <ha.s.iqbal@outlook.com>
2023-05-03 21:21:19 +0100

    added edit option

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # add a match case block
 
-user_prompt = "Type add, view or exit: "
+user_prompt = "Type add, view, edit or exit: "
 name_prompt = "Enter your name:"
+edit_prompt =  "Number of item in list to edit:"
+new_name = "What is the new name:"
 
 names = []
 while True:
@@ -15,6 +17,11 @@ while True:
         case 'view':
             for items in names:
                 print (items)
+        case 'edit':
+            edit_number = int(input(edit_prompt))
+            edit_number = edit_number - 1
+            edited_name = input(new_name)
+            names[edit_number] = edited_name
         case 'exit':
             break
 
