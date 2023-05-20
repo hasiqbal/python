e671a9d4cf92336ab99c4b4e33c4d9ddeecfe7e3
hasiqbal <ha.s.iqbal@outlook.com>
2023-05-20 18:30:32 +0100

    added functions

---
+++ name-list-app/day10/main.py
@@ -0,0 +1,85 @@
+user_prompt = "Type add, show, edit, remove, or exit: "
+name_prompt = "Enter your name:"
+edit_prompt = "Number of item in the list to edit?:"
+new_name = "What is the new name?:"
+remove_name = "Which number name to remove?:"
+
+def get_names():
+    with open('names.txt', 'r') as file:
+        names = file.readlines()
+    return names
+
+def write_names():
+    with open('names.txt', 'w') as file:
+        file.writelines(names)
+    return names
+
+
+
+while True:
+    name = input(user_prompt)
+    name = name.strip()
+    
+
+    if name.startswith('add'):
+        newName = name[4:]
+
+        names = get_names()
+
+        names.append(newName)
+
+        with open('names.txt', 'w') as file:
+            file.writelines(names)
+
+    elif name.startswith('show'):
+        names = get_names()
+
+        for index, item in enumerate(names):
+            item = item.strip("\n")
+            row = f"{index+1} - {item}"
+            print(row)
+
+    elif name.startswith('edit'):
+        try: 
+            edit_number = int(name[5:])
+            edit_number = edit_number - 1
+
+            names = get_names()
+            
+            edited_name = input(new_name)
+            names[edit_number] = edited_name + '\n'
+
+            with open('names.txt', 'w') as file:
+                file.writelines(names)
+        except ValueError:
+            print('Invalid value')
+            continue
+
+    elif name.startswith('remove'):
+        try: 
+            number_in_list = int(name[7:])
+
+            names = get_names()
+
+            names = [name.strip('\n') for name in names]
+
+            index = (number_in_list - 1)
+            name_to_remove = names[index].strip('\n')
+            names.pop(index)
+
+            with open('names.txt', 'w') as file:
+                file.writelines(names)
+
+            message = f"Name {name_to_remove} was removed from the list"
+            print(message)
+        
+        except IndexError:
+            print('Index not in list')
+            continue
+
+    elif name.startswith('exit'):
+        break
+    else:
+        print("Command not valid")
+
+print("This is your final list:", get_names(), "Goodbye!")
